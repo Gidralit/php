ebf44c6cc0c223e9387b5d9e08e07cab6efc8651
Gidralit <feodorkrutkin23@gmail.com>
2024-11-15 15:46:57 +0700

    lesson 8

---
+++ lvl3/lesson8/index.php
@@ -0,0 +1,14 @@
+<?php
+    echo "!1 ".(var_dump(!1))."<br>";
+    echo "!0 ".(var_dump(!0))."<br>";   
+    echo "!true ".(var_dump(!true))."<br>";
+    echo "2 && 3 ".(var_dump(2 && 3))."<br>";
+    echo "5 && 0 ".(var_dump(5 && 0))."<br>";
+    echo "3 || 0 ".(var_dump(3 || 0))."<br>";
+    echo "5 / 1 ".(var_dump(5 / 1))."<br>";
+    echo "1/5 ".(var_dump(1/5))."<br>";
+    echo "5+'5string' ".(var_dump(5+'5string'))."<br>";
+    echo "'5'==5 ".(var_dump('5'==5)).'<br>';
+    echo "'05'==5 ".(var_dump('05'==5)).'<br>';
+    echo "'05'=='5' ".(var_dump('05'=='5'));
+    ?>
