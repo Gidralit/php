5cc6f4cd2b8137e7bf641dd0a06b588255c391a5
Gidralit <feodorkrutkin23@gmail.com>
2024-11-19 13:13:44 +0700

    lvl 3 complete

---
+++ lvl3/lesson12/index.php
@@ -0,0 +1,7 @@
+<?php
+$content = '<h1>Заголовок статьи</h1><p>Текст какой-то статьи</p>';
+$header = include 'header.php';
+$sidebar = include 'sidebar.php';
+$content = include 'content.php';
+$footer = include 'footer.php';
+?>
